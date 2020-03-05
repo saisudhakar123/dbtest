63bcad5c27143642f84e2e019284886bd484ea88
raghuapr <47568274+raghuapr@users.noreply.github.com>
2020-03-05 18:27:20 +0530

    3rd Update

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 <html>
 <body>
 
-<h1>
-Hello this is Raghavendra AP
+<h1> Hello this is Raghavendra AP </h1>
 
-Welcome to OpenShift Version3.11 !!
+<h1> Welcome to OpenShift Version3.11 !! </h1>
 
-This is only for testing purpose
+<h1> This is only for testing purpose </h1>
 
-Try /dbtest.php extension :-)
-  </h1>
+<h1> Try /dbtest.php extension :-) </h1>
+
 
 </body>
 </html>
