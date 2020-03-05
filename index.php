0951f9c848e483b7bcf1161eb21b5b448cd9df0e
raghuapr <47568274+raghuapr@users.noreply.github.com>
2020-03-05 18:25:17 +0530

    2nd update

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
 Hello this is Raghavendra AP
 
 Welcome to OpenShift Version3.11 !!
@@ -5,3 +10,7 @@ Welcome to OpenShift Version3.11 !!
 This is only for testing purpose
 
 Try /dbtest.php extension :-)
+  </h1>
+
+</body>
+</html>
